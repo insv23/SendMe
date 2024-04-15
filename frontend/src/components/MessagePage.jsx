de6ceb7ba9b0e76e2aa634877cef06ad9ed03bca
insv <insv23@outlook.com>
2024-04-16 00:22:16 +0800

    refactor: Remove unused InputBox component

--- frontend/src/components/MessagePage.jsx
+++ frontend/src/components/MessagePage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import InputBox from './InputBox';
 import MessageList from './MessageList';
 import messageService from '../services/message';
 
