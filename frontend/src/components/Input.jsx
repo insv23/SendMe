8b507430af1fbb24389ab69216bbbbcbb7ca4788
insv <insv23@outlook.com>
2024-04-15 22:12:42 +0800

    frontend/feat: add text state

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
+
 import InputEditor from "./InputEditor";
 import InputFileArea from "./InputFileArea";
 import InputToolbar from "./InputToolbar";
 
 const Input = () => {
+  const [text, setText] = useState("");
 
   return (
     <div className="input-container p-4 m-2 bg-[#202020] rounded-md border border-gray-600 hover:border-blue-500 focus-within:border-blue-500">
-      <InputEditor/>
+      <InputEditor  text={text} setText={setText} />
       <InputFileArea />
       <InputToolbar/>
     </div>
