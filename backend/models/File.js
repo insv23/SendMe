d3040542de40080e736dfcd5ba60bfcb3ff55fe5
insv <insv23@outlook.com>
2024-04-26 13:00:23 +0000

    feat: Implement deletion of files linked to messages

--- backend/models/File.js
+++ backend/models/File.js
@@ -1,14 +1,19 @@
 const db = require("../utils/db");
+const { deleteFile } = require("../utils/file");
 
 async function createFile(messageId, file) {
   const sql = `INSERT INTO File (message_id, file_type, file_size, file_name, file_original_name) VALUES (?, ?, ?, ?, ?)`;
   return new Promise((resolve, reject) => {
-    db.run(sql, [messageId, file.mimetype, file.size, file.filename, file.originalname], (err) => {
-      if (err) {
-        reject(err);
+    db.run(
+      sql,
+      [messageId, file.mimetype, file.size, file.filename, file.originalname],
+      (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(this.lastID);
       }
-      resolve(this.lastID);
-    });
+    );
   });
 }
 
@@ -25,4 +30,35 @@ async function getOriginalFileNameByFileName(fileName) {
   });
 }
 
-module.exports = { createFile, getOriginalFileNameByFileName };
+async function deleteFileByMessageId(messageId) {
+  // 找到messageId对应的所有file
+  let sql = `SELECT file_name FROM File WHERE message_id = ?`;
+  const files = await new Promise((resolve, reject) => {
+    db.all(sql, [messageId], (err, files) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(files);
+    });
+  });
+
+  // 删除文件系统中的文件
+  await Promise.all(files.map((file) => deleteFile(file.file_name)));
+
+  // 删除数据库中的文件记录
+  sql = `DELETE FROM File WHERE message_id = ?`;
+  return new Promise((resolve, reject) => {
+    db.run(sql, [messageId], (err) => {
+      if (err) {
+        reject(err);
+      }
+      resolve();
+    });
+  });
+}
+
+module.exports = {
+  createFile,
+  getOriginalFileNameByFileName,
+  deleteFileByMessageId,
+};
